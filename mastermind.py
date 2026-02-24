3a1d90eba4abf5f5da7fb03ae53229fd3575cb52
987654321gv <gabriel.sachsgandon@gmail.com>
2026-02-24 15:54:44 +0100

    modularisation
    Le nombre de couleurs et d'emplacements peut désormais être modifié

--- mastermind.py
+++ mastermind.py
@@ -3,6 +3,7 @@
 """
 from tkinter import *
 import random
+from math import sqrt,ceil
 class mastermind(Frame):
     def __init__(self,boss=None):
         Frame.__init__(self,boss)
@@ -10,7 +11,7 @@ class mastermind(Frame):
         #### valeurs arbitraires ####
         self.couleurs=['#ffffff','#000000','#ff0000','#00ff00','#0000ff','#ffff00']
         self.couleur_vide='#553823'
-        self.nb_emplacements=4
+        self.nb_emplacements=7
         self.dico_reponce = {0:'#ffffff',1: '#000000'}
         #### initialisations ####
         self.emplacements=[]
@@ -22,20 +23,29 @@ class mastermind(Frame):
         #### valeurs précalculées ####
         self.nb_couleurs=len(self.couleurs)
         self.nb_max=max(self.nb_emplacements,self.nb_couleurs-1)
-        self.endroit_emplacement=abs(self.nb_couleurs - self.nb_emplacements) // 2
+        self.endroit_emplacement=max((self.nb_couleurs - self.nb_emplacements) // 2,1)
+        self.endroit_couleurs = max((self.nb_emplacements - self.nb_couleurs) // 2, 0)
+        if self.nb_couleurs<=self.nb_emplacements:self.endroit_couleurs+=1
+        self.fin_couleurs=self.endroit_couleurs+self.nb_couleurs-1
+        self.side=ceil(sqrt(self.nb_emplacements))
+        self.coins=tuple((10,55))
+        if self.side>2:
+            self.coins=tuple(10+(((55-self.side*5)*i)//(self.side-1)) for i in range(self.side))
         #### intialisation GUI ####
         for i in range(self.nb_emplacements):
+
             self.emplacements_prec_essai.append(Frame(self, height=75,width=75))
             self.emplacements_prec_essai[-1].grid(row=1, column=self.endroit_emplacement + i, sticky=NSEW)
             self.emplacements.append(Frame(self, height=75,width=75, bg=self.couleur_vide))
             self.emplacements[-1].grid(row=0, column=self.endroit_emplacement + i, sticky=EW)
         for i,c in enumerate(self.couleurs):
-            Button(self, background=c,width=10,height=2,command=lambda couleur=i:self.jouer(couleur)).grid(row=2, column=i, sticky=EW)
-        Button(self,text='annuler', command=self.annuler).grid(row=3, column=self.nb_max//2,columnspan=1 if self.nb_max%2 else 2)
-        Button(self, text='rejouer', command=self.rejouer).grid(row=3, column=0)
-        Button(self, text='quiter', command=self.quit).grid(row=3, column=self.nb_max)
+            Button(self, background=c,width=10,height=2,command=lambda couleur=i:self.jouer(couleur)).grid(row=2, column=i+self.endroit_couleurs, sticky=EW)
+        Button(self,text='annuler', command=self.annuler).grid(row=3, column=self.nb_max//2,columnspan=1 if self.nb_couleurs%2 else 2)
+        Button(self, text='rejouer', command=self.rejouer).grid(row=3, column=self.endroit_couleurs)
+        Button(self, text='quiter', command=self.quit).grid(row=3, column=self.fin_couleurs)
         self.ale=Button(self,text='code aléatoire', command=self.rand)
-        self.ale.grid(row=4,column=self.nb_max//2,columnspan=1 if self.nb_max%2 else 2)
+        self.ale.grid(row=4,column=self.nb_max//2,columnspan=1 if self.nb_couleurs%2 else 2)
+
     def jouer(self,couleur):
         self.emplacements[self.emplacement_actif].configure(bg=self.couleurs[couleur])
         self.emplacement_actif+=1
@@ -56,12 +66,16 @@ class mastermind(Frame):
                 elif e in self.reponce:
                     rep.append(0)
                 else:rep.append(None)
+            rep.extend([None]*((self.side**2)-len(rep)))
             self.can.delete(ALL)
-            coins=(10,55)
+
+
             random.shuffle(rep)
             for i,p in enumerate(rep):
                 if p!=None:
-                    self.can.create_oval(coins[i%2],coins[i//2],coins[i%2]+10,coins[i//2]+10,fill=self.dico_reponce[p])
+                    self.can.create_oval(self.coins[i%self.side],self.coins[i//self.side],
+                                         self.coins[i%self.side]+self.side*5,self.coins[i//self.side]+self.side*5,
+                                         fill=self.dico_reponce[p])
         else:
             self.enregister_reponce(self.prec_essai)
         self.wipe_prec_essai()
@@ -87,6 +101,7 @@ class mastermind(Frame):
         self.enregister_reponce([random.randint(0,self.nb_couleurs-1) for _ in range(len(self.emplacements))])
         self.wipe_prec_essai()
         self.essais = 0
+        self.emplacement_actif=0
     def enregister_reponce(self,reponse):
         self.master.title('jeu')
         self.reponce = reponse
@@ -99,6 +114,6 @@ class mastermind(Frame):
         self.prec_essai = []
 if __name__ == '__main__':
     f = Tk()
-    #f.resizable(width=0,height=0)
+    f.resizable(width=0,height=0)
     mastermind(f)
     f.mainloop()
